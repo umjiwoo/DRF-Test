5d872892d54bec7dae0f54a0c7920cbbd287e435
sa46lll <62706048+sa46lll@users.noreply.github.com>
2021-07-31 15:58:51 +0900

    Revert "Booking 시스템 (CRUD 문서자동화)"

--- booking/views.py
+++ booking/views.py
@@ -1,13 +1,3 @@
-from rest_framework import generics
-from .models import Booking
-from .serializers import BookingSerializer
+from django.shortcuts import render
 
-
-class BookingList(generics.ListCreateAPIView):
-    queryset = Booking.objects.all()
-    serializer_class = BookingSerializer
-
-
-class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Booking.objects.all()
-    serializer_class = BookingSerializer
+# Create your views here.
--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking',
-    'rest_framework',
-    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -126,7 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
-}
